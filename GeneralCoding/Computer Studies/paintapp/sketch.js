29da7afec1a51fc2c043e1a1688dd47c7aac1e08
nianny <nianny@Mattycom.localdomain>
2021-02-15 14:51:59 +0800

    starting on paint app

---
+++ GeneralCoding/Computer Studies/paintapp/sketch.js
@@ -0,0 +1,31 @@
+let a = [];
+let b = [];
+// class freehand {
+//   constructor()
+// }
+
+function setup() {
+  createCanvas(400, 400);
+  noFill();
+  b.push(a);
+}
+        
+
+function draw() {
+  background(250);
+  //let a = [];
+  if (mouseIsPressed == true){
+    b[b.length-1].push([mouseX, mouseY]);
+
+  }
+  else {
+    b.push([]);
+  }
+  for (let p=0; p<b.length; p++){
+    beginShape();
+    for (let i=0; i<b[p].length; i++){
+      vertex(b[p][i][0], b[p][i][1]);
+    }
+    endShape();
+  }
+}
