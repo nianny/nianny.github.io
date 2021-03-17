753b0cd3b518f53d649007b829a17e4f05d23bcb
nianny <75959161+nianny@users.noreply.github.com>
2021-03-17 18:32:02 +0800

    swift lesson 1

--- GeneralCoding/Swift/Clicker/Clicker/ViewController.swift
+++ GeneralCoding/Swift/Clicker/Clicker/ViewController.swift
@@ -11,16 +11,38 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var number: UILabel!
     @IBOutlet weak var clickbutton: UIButton!
+    @IBOutlet weak var restart: UIButton!
+    @IBOutlet weak var control: UISegmentedControl!
     var counter = 0
+    var choice = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         clickbutton.layer.cornerRadius=10
+        restart.layer.cornerRadius=10
+        choice = control.selectedSegmentIndex
+        
         // Do any additional setup after loading the view.
     }
 
+//    @IBAction func choose(_ sender: Any) {
+//        choice = control.selectedSegmentIndex
+//    }
+    @IBAction func control(_ sender: Any) {
+        choice = control.selectedSegmentIndex
+    }
+    @IBAction func restart(_ sender: Any) {
+        counter = 0
+        number.text = "\(counter)"
+    }
     @IBAction func buttonPressed(_ sender: Any) {
-        counter+=1
+        if (choice == 0){
+            counter += 1
+        }
+        else if (choice == 1){
+            counter += Int.random(in: -5...10)
+        }
+        //counter+=1
         number.text = "\(counter)"
         
     }
