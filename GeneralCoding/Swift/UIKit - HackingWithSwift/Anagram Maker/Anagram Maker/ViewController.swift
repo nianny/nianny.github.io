cad2d97ecd23c1c8039ec490d779fac62cba1d11
nianny <75959161+nianny@users.noreply.github.com>
2021-04-02 02:02:39 +0800

    day 28

--- GeneralCoding/Swift/UIKit - HackingWithSwift/Anagram Maker/Anagram Maker/ViewController.swift
+++ GeneralCoding/Swift/UIKit - HackingWithSwift/Anagram Maker/Anagram Maker/ViewController.swift
@@ -57,7 +57,62 @@ class ViewController: UITableViewController {
     }
     
     func submit(_ answer: String){
+        let lowerAnswer = answer.lowercased()
+        var errorTitle: String
+        var errorMessage: String
+        if isPossible(word: lowerAnswer){
+            if isOriginal(word: lowerAnswer){
+                if isRead(word: lowerAnswer){
+                    usedWords.insert(lowerAnswer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic) // more effecient/easier
+                    // allows animatino
+                    
+                    return
+                } else {
+                    errorTitle = "Word not recognised"
+                    errorMessage = "You can't just make them up, you know!"
+                }
+            } else {
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
+            }
+        } else {
+            guard let title = title?.lowercased() else { return }
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title)"
+        }
         
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac,animated: true)
+        
+    }
+    
+    func isPossible(word: String) -> Bool{
+        guard var tempword = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let position = tempword.firstIndex(of: letter){
+                tempword.remove(at: position)
+            }
+            else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isOriginal (word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isRead (word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound // i.e. valid word
     }
 
 
