741f0feb1c0539eadf5e7b8bafaecc4887ca5d44
nianny <75959161+nianny@users.noreply.github.com>
2021-04-04 12:15:19 +0800

    basic joke app complete

--- GeneralCoding/Swift/Joke App/Joke App/ViewController.swift
+++ GeneralCoding/Swift/Joke App/Joke App/ViewController.swift
@@ -8,22 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    var currentJoke = 0
     var setups = ["Why did the chicken cross the road?", "Why couldn't the bicycle stand up?", "Is this pool safe for diving?"]
     var punchlines = ["To get to the other side!", "It was two tired!", "It deep ends"]
 
     @IBOutlet weak var noIdea: UIButton!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var continueLabel: UILabel!
+    @IBOutlet var tapButton: UITapGestureRecognizer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         noIdea.layer.cornerRadius = 10
-        questionLabel.text = setups[0]
-        answerLabel.text = punchlines[0]
+        setJoke()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func answerRequested(_ sender: Any) {
+        answerLabel.isHidden = false
+        continueLabel.isHidden = false
+        tapButton.isEnabled = true
+        noIdea.isEnabled = false
+    }
+    
+    @IBAction func screenTapped(_ sender: Any) {
+        currentJoke += 1
+        if currentJoke == setups.count{
+            currentJoke = 0
+        }
+        
+        setJoke()
+    }
+    
+    func setJoke(){
+        tapButton.isEnabled = false
+        noIdea.isEnabled = true
+        answerLabel.isHidden = true
+        continueLabel.isHidden = true
+        questionLabel.text = setups[currentJoke]
+        answerLabel.text = punchlines[currentJoke]
+    }
+    
 }
 
