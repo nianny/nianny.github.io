e144f1eca7ef41b0ac11d45fb4a57cbe2f9b4d54
nianny <75959161+nianny@users.noreply.github.com>
2021-04-04 19:21:52 +0800

    Update sketch.js

--- GeneralCoding/Computer Studies/proj1/sketch.js
+++ GeneralCoding/Computer Studies/proj1/sketch.js
@@ -3,22 +3,22 @@
 //if necessary, please remember to reload so as to allow the fonts to laod in
 
 
-let alpha = 180;
-let backgroundocolour = 220;
+let alpha = 180; // a variable that is sometimes used sometimes not (try not to change)
+let backgroundocolour = 220; // sets the background colour to be used in multiple places
 
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400); //creates canvas
 }
 
 function drawBackground() {
-  for (let i=0; i<100; i++){
+  for (let i=0; i<100; i++){ //draws a hundred random dots in places
     fill(100,100,100,alpha);
     ellipse(floor(random(0,width)), floor(random(0,height)), ((width+height)/2)/400, ((width+height)/2)/400);
   }
 }
 
-function drawSun(){
+function drawSun(){ //draws the sun (a yellow circles)
   strokeWeight(3);
   fill(200,200,100);
   ellipse(width/2, height/2, width/400*100);
@@ -28,42 +28,25 @@ function drawSun(){
 
 function drawmountains() {
   stroke(0,0,0);
-  //fill(100,100,100, alpha);
-
-  beginShape();
+  
+  //mains shape of the mountains
+  beginShape(); 
   vertex(width/7, height);
   vertex(width/3*1, height/2*1.2); 
   vertex(width/3*2.5, height);
   endShape(CLOSE);
-
+  
   beginShape();
   vertex(width/20*12, height);
-
   vertex(width/20*18, height/20*12);
   vertex(width*1.4, height);
   endShape(CLOSE);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   beginShape();
   vertex(0, height);
   vertex(width, height);
   vertex(width, height/1.15)
   vertex(width*0.8, height/1.6)
-
   vertex(width/1.5, height/2);
   vertex(width/2, height/1.7)
   vertex(width/3.2, height/1.3)
@@ -71,6 +54,7 @@ function drawmountains() {
   endShape(CLOSE);
 
   
+  //shadings
 
   line(width/3.2, height/1.3, width/10*0.3, height);
 
@@ -87,7 +71,6 @@ function drawmountains() {
   vertex(width/20*6.2, height/10*7.55);
   vertex(width/20*6.7, height/20*14.6);
   endShape(CLOSE);
-  //triangle(width/20*6.6, height/10*6.3, width/20*5.7, height/10*7.4, width/20*6.3, height/10*7.55);
 
   fill(150,alpha);
   beginShape();
@@ -101,9 +84,7 @@ function drawmountains() {
 
   fill(230,alpha);
   triangle(width/20*4.7, height/20*15, width/20*4.7, height/20*16.25, width/20*5.75, height/20*15.5);
-
   triangle(width/20*6.8, height/20*12.6, width/20*6.9, height/20*14.4, width/20*7.9, height/20*13.5);
-
   triangle(width/20*17.9, height/20*12.55, width/20*17.2, height/20*13.5, width/20*17.9, height/20*14.5)
 
   fill(120,alpha);
@@ -114,19 +95,13 @@ function drawmountains() {
   vertex(width/20*2, height);
   vertex(width/2, height/20*12.2);
   endShape(CLOSE);
-
-
-
-  
-  
   stroke(0);
 }
 
 
 
-function writeWords(){
+function writeWords(){ //write words (top left corner)
   fill(0,0,0,alpha);
-  //textAlign (CENTER);
   textFont('Potta One');
   textSize(((width+height)/2)/10);
   text("Horizon", width/30,height/8);
@@ -141,7 +116,7 @@ function writeWords(){
 
 
   
-function draw() {
+function draw() { //draws everything in order
   background(backgroundocolour);
   drawBackground();
 
@@ -154,5 +129,5 @@ function draw() {
   
 
   writeWords();
-  noLoop();
+  noLoop(); //static image + avoid the random dots from flickering everywhere
 }
