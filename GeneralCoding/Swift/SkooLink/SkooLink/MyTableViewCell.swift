245e2e2f38f18c0bf457b09bde00f9331223e87e
nianny <75959161+nianny@users.noreply.github.com>
2021-04-10 18:31:57 +0800

    fudge tryiung to follow video about custom tableview

--- GeneralCoding/Swift/SkooLink/SkooLink/MyTableViewCell.swift
+++ GeneralCoding/Swift/SkooLink/SkooLink/MyTableViewCell.swift
@@ -8,7 +8,19 @@
 import UIKit
 
 class MyTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var userName: UIButton!
+    @IBOutlet weak var postLabel: UILabel!
+    static let identifier = "MyTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+    
+    public func configure(with username: String, imageName: String){
+        userName.setTitle(username, for: .normal)
+        profilePicture.image = UIImage(systemName: imageName)
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- GeneralCoding/Swift/SkooLink/SkooLink/ViewController.swift
+++ GeneralCoding/Swift/SkooLink/SkooLink/ViewController.swift
@@ -7,90 +7,33 @@
 import UIKit
 import Firebase
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     let arr = ["Chat 1", "Chat 2", "Chat 3", "Chat 4"]
+    
+    @IBOutlet var table: UITableView!
     override func viewDidLoad() {
+        super.viewDidLoad()
+        table.register(MyTableViewCell.nib(), forCellReuseIdentifier: MyTableViewCell.identifier)
+        table.delegate = self
+        table.dataSource = self
         self.title = "Chats"
         navigationController?.navigationBar.prefersLargeTitles = true
-        
-        super.viewDidLoad()
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
-    // MARK: - Table view data source
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-//
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 3
     }
 
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Posts", for: indexPath)
-        cell.textLabel?.text = arr[indexPath.row]
-        return cell
-    }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        if let vc = storyboard?.instantiateViewController(identifier: "Profile") as? ProfileViewController {
-//            vc.selectedUser = uid[indexPath.row]
-//            navigationController?.pushViewController(vc, animated: true)
-//        }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let customCell = tableView.dequeueReusableCell(withIdentifier: MyTableViewCell.identifier, for: indexPath) as! MyTableViewCell
+        customCell.configure(with: "Nianny", imageName: "")
+        return customCell
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "Posts", for: indexPath)
+//
+//
+//        cell.textLabel?.text = "Hello world"
+//        return cell
     }
 }
 
