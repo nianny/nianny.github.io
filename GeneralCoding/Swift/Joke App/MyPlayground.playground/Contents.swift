602be2bfb50662cc6619e482563f664b60384ece
nianny <75959161+nianny@users.noreply.github.com>
2021-04-17 11:10:13 +0800

    help jr

--- GeneralCoding/Swift/Joke App/MyPlayground.playground/Contents.swift
+++ GeneralCoding/Swift/Joke App/MyPlayground.playground/Contents.swift
@@ -2,35 +2,69 @@ import UIKit
 import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-var str = "Hello, playground"
-let url = URL(string: "https://icanhazdadjoke.com/")!
-var request = URLRequest(url: url)
+//var str = "Hello, playground"
+//let url = URL(string: "https://icanhazdadjoke.com/")!
+//var request = URLRequest(url: url)
+//request.httpMethod = "GET"
+//request.setValue("application/json", forHTTPHeaderField:"Accept")
+//let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
+//    guard let data = data else { return }
+////    print(String(data: data, encoding: .utf8)!)
+//    var result: Response?
+//    do {
+//        result = try JSONDecoder().decode(Response.self, from: data)
+//    }
+//    catch {
+//        print("Fail")
+//    }
+//    if let joke = result {
+//        print(joke.joke)
+//        print(type(of: joke.joke))
+//        print(joke.id)
+//        print(type(of: joke.id))
+//    } else{
+//        print("Fail")
+//    }
+////    print(result?.joke)
+//
+//}
+//
+//task.resume()
+//
+//
+
+let url = URL(string: "https://icanhazdadjoke.com/")
+var request = URLRequest(url: url!)
 request.httpMethod = "GET"
-request.setValue("application/json", forHTTPHeaderField:"Accept")
-let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
-    guard let data = data else { return }
-//    print(String(data: data, encoding: .utf8)!)
+request.setValue("application/json", forHTTPHeaderField: "Accept")
+        
+let task = URLSession.shared.dataTask(with: request, completionHandler: { data, response, error in
+    guard let data = data, error == nil else{
+        print("Something went wrong")
+        return
+    }
+            
+    //converting data
     var result: Response?
-    do {
+    do{
         result = try JSONDecoder().decode(Response.self, from: data)
+    } catch {
+        print("failed to convert")
+        print(data)
     }
-    catch {
-        print("Fail")
-    }
-    if let joke = result {
-        print(joke.joke)
-        print(type(of: joke.joke))
-        print(joke.id)
-        print(type(of: joke.id))
+            
+    if let json = result{
+        print("hm")
+        print(json.status)
+        print(json.joke)
+        print(json.id)
     } else{
-        print("Fail")
+        return
     }
-//    print(result?.joke)
+            
 
-}
-
+})
 task.resume()
-
 
 struct Response: Codable{
     let id: String
