931df4e20c1c4b68d71de1e5fd066b50236a4136
nianny <75959161+nianny@users.noreply.github.com>
2021-04-18 23:33:05 +0800

    yay rotarylock

--- GeneralCoding/CP/candies/main.cpp
+++ GeneralCoding/CP/candies/main.cpp
@@ -1,9 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-
+int n;
+int maximum = 1000005;
+int ft[1000005];
+int ls (int x){ //finds least sig bit
+    return (x & (-x));
+}
+void update(int l, int r, int v){
+    r++; //update differences at r+1, not r
+    for(; l <= maximum; l+= ls(l)) ft[l] = (ft[l] + v);
+    for(; r <= maximum; r+= ls(r)) ft[r] = (ft[r] + v);
+}
+int query(int p){
+    int sum = 0;
+    for (; p; p -= ls(p)) sum = (sum + ft[p]);
+    return sum;
+}
 int32_t main() {
     ios_base::sync_with_stdio(); cin.tie(0); cout.tie(0); 
-    
+    cin>>n;
     return 0;
 }
--- GeneralCoding/CP/rotarylock/main.cpp
+++ GeneralCoding/CP/rotarylock/main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+int modulo=1;
 int n,q,k;
 int ft[1000005];
 int ls (int x){ //finds least sig bit
@@ -8,17 +9,17 @@ int ls (int x){ //finds least sig bit
 }
 void update(int l, int r, int v){
     r++; //update differences at r+1, not r
-    for(; l <= n; l+= ls(l)) ft[l] += v;
-    for(; r <= n; r+= ls(r)) ft[r] -= v;
+    for(; l <= n; l+= ls(l)) ft[l] = (ft[l] + v)% modulo;
+    for(; r <= n; r+= ls(r)) ft[r] = (ft[r] + v) % modulo;
 }
 int query(int p){
     int sum = 0;
-    for (; p; p -= ls(p)) sum += ft[p];
+    for (; p; p -= ls(p)) sum = (sum + ft[p]) % modulo;
     return sum;
 }
 int32_t main() {
     ios_base::sync_with_stdio(); cin.tie(0); cout.tie(0); 
-    int modulo=1;
+    
     cin>>n>>q>>k;
     for (int i=0; i<k; i++){
         modulo *= 2;
@@ -34,14 +35,23 @@ int32_t main() {
         cin>>a;
         if (a==0){
             cin>>b>>c;
-            cout<<a<<' '<<b<<' '<<c<<' '<<'\n';
-            update(0,b,c);
+            // cout<<a<<' '<<b<<' '<<c<<' '<<'\n';
+            update(b,n,c);
         }
         else {
             cin>>b;
-            cout<<"HALLO";
+            // cout<<"HALLO";
             cout<<query(b) % modulo<<'\n';
         }
     }
     return 0;
 }
+/*
+4 6 100000
+0 3 2
+1 2
+1 4
+0 2 1
+1 2
+1 3
+*/
