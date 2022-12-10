66e463a335a2f93af908e803e54e6fbcccebff54
nianny <xiaonianhe@hotmail.com>
2022-12-10 19:26:59 +0800

    inversions yay

--- GeneralCoding/CP/Codebreaker/inversions/main.cpp
+++ GeneralCoding/CP/Codebreaker/inversions/main.cpp
@@ -5,7 +5,52 @@ using namespace std;
 #define endl '\n'
 int n;
 int arr[50010];
+int c;
+void merge(int st, int en){
+   
+    if (st == en) return;
+    int mid = (st + en)/2;
+    int sz = en - st + 1;
 
+    merge(st, mid);
+    merge(mid+1, en);
+
+    int arr2[sz];
+    int pos1 = st; //0
+    int pos2 = mid+1; //1
+    //cout<<st<<' '<<en<<' '<<mid<<' '<<pos1<<' '<<pos2<<'\n';
+
+    for (int i=0; i<sz; i++){
+        if (pos1>mid){
+            arr2[i] = arr[pos2];
+            pos2++;
+        }
+        else if (pos2 > en){
+            arr2[i] = arr[pos1];
+            pos1++;
+        }
+        else if (arr[pos1] <= arr[pos2]){
+            arr2[i] = arr[pos1];
+            pos1++;
+        }
+        else{
+            arr2[i] = arr[pos2];
+            c += pos2-i-st;
+            
+            //cout<<st<<' '<<en<<' '<<mid<<' '<<pos1<<' '<<pos2<<' '<<c<<' '<<i<<'\n';
+            pos2++;
+        }
+    }
+
+    for (int j = 0; j < sz; j++) arr[j + st] = arr2[j];
+    /*
+    for (int i=0; i<n; i++){
+		cout<<arr[i]<<' ';
+	}
+	cout<<'\n';
+	* */
+	
+}
 int32_t main() {
     // ifstream cin("addin.txt");
     // ofstream cout("addout.txt");
@@ -17,8 +62,23 @@ int32_t main() {
         for (int i=0; i<n; i++){
             cin>>arr[i];
         }
+        /*
+        for (int i=0; i<n; i++){
+            cout<<arr[i]<<' ';
+        }
+        cout<<'\n';
+        * */
         
-
+        c = 0;
+        merge(0, n-1);
+        cout<<c<<'\n';
+        /*
+        for (int i=0; i<n; i++){
+            cout<<arr[i]<<' ';
+        }
+        cout<<'\n';
+        * */
+        
     }
     
     return 0;
