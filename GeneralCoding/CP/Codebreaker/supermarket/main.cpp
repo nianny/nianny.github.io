4a4e386ddeff11ee14973a12e0b958a63d4d4e0e
nianny <xiaonianhe@hotmail.com>
2023-01-04 21:14:48 +0800

    peepeepoopoo

--- GeneralCoding/CP/Codebreaker/supermarket/main.cpp
+++ GeneralCoding/CP/Codebreaker/supermarket/main.cpp
@@ -4,37 +4,71 @@ using namespace std;
 #define hallo ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define endl '\n'
 
-
+//peepeepoopoo
 int32_t main() {
     // ifstream cin("addin.txt");
     // ofstream cout("addout.txt");
     hallo;
     int n,x;
     cin>>n>>x;
-    int D[n];
-    int W[n];
-    for (int i=0; i<n; i++){
+    int D[n+5];
+    int W[n+5];
+    for (int i=1; i<=n; i++){
         cin>>D[i];
     }
 
-    for (int i=0; i<n; i++){
+    for (int i=1; i<=n; i++){
         cin>>W[i];
     }
-    int ss[n];
-    ss[0] = D[0];
-    for (int i=1; i<n; i++){
-        ss[i] = ss[i-1]+D[i];
+    int deli[n+5];
+    int weig[n+5];
+    // ss[0] = D[0];
+    deli[0] = 0;
+    weig[0] = 0;
+    for (int i=1; i<=n; i++){
+        deli[i] = deli[i-1]+D[i];
+        weig[i] = weig[i-1]+ W[i];
     }
 
-    int l=0;
-    int r=0;
+    // int l=1;
+    int r=1;
 
-    int sum = 0;
-    deque<int>dq;
+    // int sum = 0;
+    // deque<int>dq;
 
-    while(r <= n){
-        
+    // while(r <= n+1){
+
+    // }
+    int rightishpotato[n+5];
+    rightishpotato[0] = 0;
+    for (int l=1; l<=n; l++){
+        while (r <= n && weig[r]-weig[l-1] <= x){
+            r++;
+        }
+
+        rightishpotato[l] = r-1;
     }
+
+    // for (int i=0; i<=n; i++){
+    //     cout<<rightishpotato[i]<<' ';
+    // }
+    // cout<<'\n';
+
+    stack<int*>badboipotatohmm;
+    badboipotatohmm.push(rightishpotato);
+    deque<int>peepeepoopoo;
+    int ans = 0;
+    for (int i=1; i<=n; i++){
+        for (int x = rightishpotato[i-1]+1; x <= rightishpotato[i]; x++){
+            while(!peepeepoopoo.empty() and deli[peepeepoopoo.back()] < deli[x]) peepeepoopoo.pop_back();
+            peepeepoopoo.push_back(x);
+        }
+
+        while(!peepeepoopoo.empty() and peepeepoopoo.front() < i) peepeepoopoo.pop_front(); 
+
+        ans = max(ans, deli[peepeepoopoo.front()]-deli[i-1]);
+    }
+    cout<<ans;
     /*
     雪花飘飘北风萧萧
     天地一片苍茫
